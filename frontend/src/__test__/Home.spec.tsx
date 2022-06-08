b53aa760aa95f90e2585799e08d9edae082ca2bf
Igor Moraes <igormoraesdev@gmail.com>
2022-06-08 15:51:02 -0300

    feat: add jest config

---
+++ frontend/src/__test__/Home.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import { HomeScreen } from '../screens'
+
+describe('HomeScreen', () => {
+  it('should be render', () => {
+    render(<HomeScreen />)
+  })
+})
