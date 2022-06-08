79b6e9d55f3be2860689c1644eafcdcd91d1c239
Igor Moraes <igormoraesdev@gmail.com>
2022-06-08 20:47:15 -0300

    chore: update textinput component

---
+++ frontend/src/components/TextInput/index.tsx
@@ -0,0 +1,29 @@
+type Props = {
+  label: string
+  name: string
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+type InputProps = HTMLInputElement | Props
+
+const TextInput = ({ label, name, onChange }: Props & InputProps) => {
+  return (
+    <div className="rounded -space-y-px">
+      <div>
+        <label htmlFor={name} className="sr-only">
+          {label}
+        </label>
+        <input
+          data-testid="textinput"
+          onChange={onChange}
+          id={name}
+          name={name}
+          className="appearance-none rounded relative block w-full px-3 py-3 border-2 font-primary border-gray-300 placeholder-gray-600 text-coolGray-900 focus:outline-none focus:ring-indigo-600 focus:border-indigo-600 focus:z-10 sm:text-md text-md"
+          placeholder={label}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default TextInput
