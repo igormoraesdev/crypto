0d9b581991dbb430fc0f252f4d1ab7689de8cdcd
Igor Moraes <igormoraesdev@gmail.com>
2022-06-12 13:20:13 -0300

    feat: add http client

---
+++ frontend/src/lib/http-client/http-client-api.ts
@@ -0,0 +1,5 @@
+import { HttpClient } from './http-client'
+
+export const httpClientApi = new HttpClient(
+  'https://075008f2-2bc9-4567-95e7-3420ce91a9ec.mock.pstmn.io'
+)
---
+++ frontend/src/lib/http-client/http-client.ts
@@ -0,0 +1,13 @@
+import axios, { Axios, AxiosRequestConfig } from 'axios'
+
+export class HttpClient {
+  httpClient: Axios
+
+  constructor(baseURL: string) {
+    this.httpClient = axios.create({ baseURL })
+  }
+
+  request(options: AxiosRequestConfig) {
+    return this.httpClient.request(options)
+  }
+}
