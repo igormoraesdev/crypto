c8e9fd4ce0b6e0aae32ebdf558201315938fec6a
Igor Moraes <igormoraesdev@gmail.com>
2022-06-12 13:20:26 -0300

    feat: update navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,6 +7,7 @@ const Navbar = () => {
   const router = useRouter()
   const isRegisterPath = router.pathname.includes('register')
   const isLoginPath = router.pathname.includes('login')
+  const isDashboardPath = router.pathname.includes('dashboard')
 
   return (
     <div className="z-50 fixed bg-white min-w-full max-w-7xl mx-auto px-4 sm:px-6 py-4 border-b-2 border-indigo-600">
@@ -30,22 +31,24 @@ const Navbar = () => {
         </Link>
 
         <div className="">
-          <div className="flex items-center justify-end md:flex-1 lg:w-0">
-            {!isLoginPath && (
-              <Link passHref href="/login">
-                <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-indigo-600 ease-in duration-300 hover:-translate-y-1">
-                  Sign in
-                </a>
-              </Link>
-            )}
-            {!isRegisterPath && (
-              <Link passHref href="/register">
-                <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-900 hover:-translate-y-1 ease-in duration-300">
-                  Sign up
-                </a>
-              </Link>
-            )}
-          </div>
+          {isDashboardPath ? null : (
+            <div className="flex items-center justify-end md:flex-1 lg:w-0">
+              {!isLoginPath && (
+                <Link passHref href="/login">
+                  <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-indigo-600 ease-in duration-300 hover:-translate-y-1">
+                    Sign in
+                  </a>
+                </Link>
+              )}
+              {!isRegisterPath && (
+                <Link passHref href="/register">
+                  <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-900 hover:-translate-y-1 ease-in duration-300">
+                    Sign up
+                  </a>
+                </Link>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
