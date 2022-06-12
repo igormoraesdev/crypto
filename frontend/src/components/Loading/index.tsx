0c2de1cc39c2082d396128a7c9e643614ceb79a5
Igor Moraes <igormoraesdev@gmail.com>
2022-06-12 13:19:47 -0300

    chore: feat add loading component

---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-20 h-20 border-l-2 border-indigo-600 rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default Loading
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar'
 export { default as TextInput } from './TextInput'
 export { default as Typography } from './Typography'
+export { default as Loading } from './Loading'
